6f9ff3c71eedbc7ab11ec2334e0b737255c35898
HIDEHIKO0522 <sirap@jcom.home.ne.jp>
2019-08-12 07:48:17 +0000

    new.html修正と部分テンプレート作成

--- app/controllers/checks_controller.rb
+++ app/controllers/checks_controller.rb
@@ -9,22 +9,17 @@ class ChecksController < ApplicationController
   def new
     # Model.find(Model.pluck(:id).shuffle[0..4])
     
-    @question1 = Question.where( category:1).order("RAND()").limit(1).map{|v| [v.text]}
-    @question2 = Question.where( category:2).order("RAND()").limit(1).map{|v| [v.text]}
-    @question3 = Question.where( category:3).order("RAND()").limit(1).map{|v| [v.text]}
-    @question4 = Question.where( category:4).order("RAND()").limit(1).map{|v| [v.text]}
-    @question5 = Question.where( category:5).order("RAND()").limit(1).map{|v| [v.text]}
+    @question1 = Question.where( category:1).order("RAND()").limit(1).map{|v| v.text}
+    @question2 = Question.where( category:2).order("RAND()").limit(1).map{|v| v.text}
+    @question3 = Question.where( category:3).order("RAND()").limit(1).map{|v| v.text}
+    @question4 = Question.where( category:4).order("RAND()").limit(1).map{|v| v.text}
+    @question5 = Question.where( category:5).order("RAND()").limit(1).map{|v| v.text}
+    
+    @check = Check.new
   end  
 
   def create
-   Check.create(pre_score: check_params[:pre_score],
-                chk_score: check_parans[:chk_score],
-                chk_score1: check_params[:chk_score1],
-                chk_score2: check_params[:chk_score2],
-                chk_score3: check_params[:chk_score3],
-                chk_score4: check_params[:chk_score4],
-                chk_score5: check_params[:chk_score5],
-                user_id: current_user.id)
+    Check.create(check_params)
   end
 
   def show
@@ -34,7 +29,7 @@ class ChecksController < ApplicationController
 
   private
   def check_params
-    params.permit(:pre_score, :chk_score, :chk_score1, :chk_score2, :chk_score3, :chk_score4, :chk_score5)
+    params.permit(:pre_score, :chk_score1, :chk_score2, :chk_score3, :chk_score4, :chk_score5, :chk_score, user_id: current_user.id)
   end  
  
   def move_to_index
