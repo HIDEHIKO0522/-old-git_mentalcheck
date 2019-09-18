68b14a12e6a7bfe7342a85bcb3b01d8997e41b33
HIDEHIKO0522 <sirap@jcom.home.ne.jp>
2019-09-18 13:05:59 +0000

    add csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "csv"
+
+questions_csv = CSV.readlines("db/questions.csv")
+questions_csv.each do |row|
+  Question.create(text: row[1], categoly: row[2],)
+end
